1794c941d62b1ea5de66bb81b7c1d78526f07978
Jaume Ballester <86907672+jaumebt90@users.noreply.github.com>
2021-11-24 10:43:53 +0100

    Update logic.js

--- logic.js
+++ logic.js
@@ -15,11 +15,11 @@ document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
   if (!guess) {
-    document.querySelector(".message").textContent = "⛔️ No Number!";
+    displayMessage("⛔️ No Number!");
 
     // When player wins
   } else if (guess === secretNumber) {
-    document.querySelector(".message").textContent = "🎊 Correct Number!";
+    displayMessage("🎊 Correct Number!");
     document.querySelector(".number").textContent = secretNumber;
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
@@ -33,10 +33,10 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".message").textContent =
         guess > secretNumber ? "⬆ Too high!" : "⬇ Too low!";
       score--;
-      document.querySelector(".score").textContent = score;
+      displayScore(score);
     } else {
       document.querySelector(".message").textContent = "💥 You lost the game!";
-      document.querySelector(".score").textContent = 0;
+      displayScore(0);
       document.querySelector("body").style.backgroundColor = "red";
       document.querySelector(".number").style.width = "30rem";
     }
@@ -46,9 +46,9 @@ document.querySelector(".check").addEventListener("click", function () {
 document.querySelector(".again").addEventListener("click", function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
-  document.querySelector(".score").textContent = score;
+  displayScore(score);
   document.querySelector("body").style.backgroundColor = "#222";
-  document.querySelector(".message").textContent = "Start guessing...!";
+  displayMessage("Start guessing...!");
   document.querySelector(".number").style.width = "15rem";
   document.querySelector(".guess").value = "";
   document.querySelector(".number").textContent = "?";
